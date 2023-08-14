91e09b2cc236034a5bd07467cef85604c5dcdf23
Roberto Guevara <robertoeguevara@gmail.com>
2023-08-13 20:01:30 -0700

    create chatStripe function (divs that will render the messages)

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/bog.svg';
+import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
@@ -29,4 +29,30 @@ function typeText(element, text) {
             clearInterval(interval);
         }
     }, 20)
+}
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe (isAi, value, uniqueId) {
+    return (
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                <div class="chat">
+                    <div className="profile">
+                        <img 
+                            src="${isAi ? bot : user}"
+                            alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+                    <div class="message" id=${uniqueId}>${value}</div>
+                </div>
+            </div>
+        `
+    )
 }
