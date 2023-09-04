642bf5537c0014369dc2f5bd75a325326f003560
Roberto Guevara <robertoeguevara@gmail.com>
2023-09-03 19:46:32 -0700

    fix: deprecated import Configuration module

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+//const openai = new OpenAIApi(openai);
 
 const app = express();
 app.use(cors());
@@ -26,13 +26,13 @@ app.post('/', async (req,res) => {
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
-            model="text-davinci-003",
-            prompt=`${prompt}`,
-            temperature=0,
-            max_tokens=3000,
-            top_p=1,
-            frequency_penalty=0.5,
-            presence_penalty=0
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
         });
 
         res.status(200).send({
